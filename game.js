7d9fdd31d577f3b47c02561936e0bb8fbaf3a654
Queary <72671469+Queary@users.noreply.github.com>
2020-10-31 16:21:06 +0300

    render + method system optimally

--- game.js
+++ game.js
@@ -1,22 +1,31 @@
 window.onload = function(){
   var x = new enigne("canvas");
-  x.add_block('gr',15,15,15,15,'red')
-  x.add_block('gr2',30,30,15,15,'red')
-  x.add_block('gr3',45,45,15,15,'red')
-  x.render_all(10);
-  x.add_block('gr4',55,55,15,15,'red')
-  var gfd=0;
-  x.move('gr3',gfd,80);
-  gfd+=10;
-  setInterval(function() {
-    x.move('gr',gfd,80);gfd+=1;
-  },10)
-  x.move('gr',gfd,80);gfd+=10;
-  x.move('gr5',gfd,80);gfd+=60;
-  x.move('gr2',gfd,80);gfd+=10;
-  x.move('gr5',gfd,80);gfd+=10;
-  x.move('gr5',gfd,80);
-  var y = new enigne("canvas");
+  x.block('gr1',50,50,50,50,'black');
+  x.block('gr2',150,150,50,50,'black');
+  x.render();
+  x.block('gr3',250,250,50,50,'#123321');
+  x.render();
+  x.move('gr1',5,5);
+  // x.clear();
+  x.render();
+  var s = x.v.x('gr1');
+  var s2= x.v.y('gr1');
+  // console.log(x.v.x('gr1'));
+  document.addEventListener('keydown', function(event) {
+    //консоль выводит всё о нажатой клавиши
+    //console.log(event)
+    if (event.key == 'd') {
+      s+=10
+      x.move('gr1',s,s2);
+      x.render();
+    }
+    if (event.key == 'a') {
+
+    }
+    if(event.key=='w'){
+
+      }
+  });
   // y.add_block('gr',85,85,15,15,'red');
   // y.render_all(100);
   // x.render(100);
